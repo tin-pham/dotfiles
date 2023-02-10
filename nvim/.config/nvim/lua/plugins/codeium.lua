01a055fabeb5ab53923dfe21e6583fbe4c5bb621
shin <heoconlinhat123@gmail.com>
2023-02-11 06:09:32 +0700

    add codeium

---
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,19 @@
+--> Github Copilot Alternative
+return {
+	'Exafunction/codeium.vim',
+	event = 'InsertEnter',
+	config = function()
+		vim.keymap.set('i', '<C-g>', function()
+			return vim.fn['codeium#Accept']()
+		end, { expr = true })
+		vim.keymap.set('i', '<c-;>', function()
+			return vim.fn['codeium#CycleCompletions'](1)
+		end, { expr = true })
+		vim.keymap.set('i', '<c-,>', function()
+			return vim.fn['codeium#CycleCompletions'](-1)
+		end, { expr = true })
+		vim.keymap.set('i', '<c-x>', function()
+			return vim.fn['codeium#Clear']()
+		end, { expr = true })
+	end,
+}
